ed1b8f5dcc2a33b10175012f176916a544d8b019
NikMassV <slipknoshka777@gmail.com>
2024-01-30 13:45:26 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './header.js'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      todos: [
+        'first',
+        'second',
+        'third'
+      ]
+    }
+  }
+  
+  addTodo = (todo) => {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        todo 
+      ]
+    })
+  }
+
+  render() {
+    return <div className="application">
+      <Header 
+        addTodoMethod={this.addTodo}
+      />
+      <Body 
+        todosArg={this.state.todos}
+      />
+    </div>
+  }
+}
+
+const Body = ({todosArg}) => {
+  return <div>
+    {todosArg.map((todo, i) => <div key={i}>{todo}</div>)}
+</div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +50,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
